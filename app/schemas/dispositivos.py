6ffc137addf58e4ed832a5d97153f2ab95905af6
m4riAj-pR <mjparrado@ucundinamarca.edu.co>
2026-03-04 23:17:18 -0500

    analitika parte 1
    esy¿ta aventura empieza con un sueño el de ser una gran programador con chapitus plus lo lograremos, pero claro que si

--- app/schemas/dispositivos.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-class Dispositivo(BaseModel):
-    id: int
-    referencia: str
-    descripcion: str
-    dispositivo_id: int
--- app/schemas/lectura.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Lectura(BaseModel):
-    id: int
-    fechahora: str
-    valor: float
-    sensor_id: int
