e2eacb9db242ae3667ee51874c7251395bd3763f
m4riAj-pR <mjparrado@ucundinamarca.edu.co>
2026-04-09 00:11:14 -0500

    actualizacion base de datos
    se actualizo la base de datos respecto a las funcionalidades (tabals de seguridad el guardian, traduccion de tablas, creacion de tablas intermedias para normalizacion)

--- app/schemas/canales.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class Canal(BaseModel):
-    id_canal: int
-    nombre: str
-    tipo: Optional[str] = None
--- app/schemas/usuarios.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Usuario(BaseModel):
-    id_usuario: int 
-    email: str
-    nombres: str
-    apellidos: str
-    rol: str
-    password_hash:str
